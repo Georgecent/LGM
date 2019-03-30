715bd81a5da9f13b595997a5e589e6b66b40a70e
Georgecent <aurorayqz@gmail.com>
2019-03-30 23:37:33 +0800

    init_introduce

--- cmd/build.go
+++ cmd/build.go
@@ -22,14 +22,8 @@ import (
 
 // buildCmd represents the build command
 var buildCmd = &cobra.Command{
-	Use:   "build",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Use:   "build [any valid 'docker build' arguments]",
+	Short: "Builds and analyzes a docker image from a Dockerfile (this is a thin wrapper for the 'docker build' command).",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("build called")
 	},
--- cmd/root.go
+++ cmd/root.go
@@ -28,14 +28,10 @@ var cfgFile string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "LGM",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Use:   "LGM [IMAGE]",
+	Short: "Docker Image Visualizer & Explorer",
+	Long: `LGM is a command line tool that can be run on Ubuntu/Debian, RHEL/Centos, Arch Linux and other platforms. 
+It is mainly used to mine Docker images, analyze layer content, and help reduce the size of Docker images.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	//	Run: func(cmd *cobra.Command, args []string) { },
