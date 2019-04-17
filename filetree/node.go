9507d380f4938d4b44e20347ad4ae16719e7a0d8
Georgecent <aurorayqz@gmail.com>
2019-04-17 22:21:11 +0800

    VisitDepthChildFirst_2

--- filetree/node.go
+++ filetree/node.go
@@ -1,6 +1,9 @@
 package filetree
 
-import "strings"
+import (
+	"sort"
+	"strings"
+)
 
 // NewNode creates a new FileNode relative to the given parent node with a payload.
 func NewNode(parent *FileNode, name string, data FileInfo) (node *FileNode)  {
@@ -48,5 +51,22 @@ func (node *FileNode) Copy(parent *FileNode) *FileNode {
 
 // VisitDepthChildFirst iterates a tree depth-first (starting at this FileNode), evaluating the deepest depths first (visit on bubble up)
 func (node *FileNode) VisitDepthChildFirst(visitor Visitor, evaluator VisitEvaluator) error {
-	
+	var keys []string
+	for key := range node.Children{
+		keys = append(keys, key)
+	}
+	sort.Strings(keys)
+	for _, name := range keys{
+		child := node.Children[name]
+		err := child.VisitDepthChildFirst(visitor, evaluator)
+		if err != nil {
+			return err
+		} else if evaluator != nil && evaluator(node) {
+			
+		}
+	}
+
+	if node == node.Tree.Root {
+		return nil
+	}
 }
